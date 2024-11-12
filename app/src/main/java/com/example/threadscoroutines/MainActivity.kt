bfb542bfae3edaee6693a9707969f56c30981c06
leonardo nascimento <leonardo.rigonini@gmail.com>
2024-11-12 15:48:43 -0300

    final commit

--- app/src/main/java/com/example/threadscoroutines/MainActivity.kt
+++ app/src/main/java/com/example/threadscoroutines/MainActivity.kt
@@ -4,7 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.threadscoroutines.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +26,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private var pararThread = false
+    private var job: Job? = null
 
+    override fun onDestroy() {
+        super.onDestroy()
+        job?.cancel()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -22,18 +40,63 @@ class MainActivity : AppCompatActivity() {
             startActivity(
                 Intent(this, SegundaActivity::class.java)
             )
+            finish()
         }
 
         binding.btnParar.setOnClickListener {
-            pararThread = true
+//            pararThread = true
+            job?.cancel()
             binding.btnIniciar.text = "Reiniciar execução"
             binding.btnIniciar.isEnabled = true
         }
 
         binding.btnIniciar.setOnClickListener {
+//            MainScope().launch{
+//            CoroutineScope(Dispatchers.IO).launch{
+//            GlobalScope.launch {
+            lifecycleScope.launch {
+                repeat(15){ indice ->
+//                    binding.btnIniciar.text="Executando $indice"
+
+                    Log.i("info_coroutine","Executando: $indice T: ${Thread.currentThread().name}")
+                    delay(1000L)
+                }
+            }
+
+           /* job = CoroutineScope(Dispatchers.IO).launch {
+//                binding.btnIniciar.text = "Executou"
+                *//*repeat(15){indice ->
+                    Log.i("info_coroutine","Executando: $indice T: ${Thread.currentThread().name}")
+
+                    withContext(Dispatchers.Main){
+                        binding.btnIniciar.text = "Executando: $indice T: ${Thread.currentThread().name}"
+                    }
+                    delay(1000)
+                }*//*
+//                recuperarUsuarioLogado()
+                *//*withTimeout(7000L){
+                    executar()
+                }*//*
+                val tempo = measureTimeMillis {
+
+                    val resultado1 = async {tarefa1()}
+
+                    val resultado2 = async {tarefa2()}
+
+                    withContext(Dispatchers.Main){
+                        binding.btnIniciar.text = "${resultado1.await()}"
+                        binding.btnParar.text = "${resultado2.await()}"
+                    }
+
+                    Log.i("info_coroutine","resultado1: ${resultado1.await()} ")
+                    Log.i("info_coroutine","resultado1: ${resultado2.await()} ")
+                }
+                Log.i("info_coroutine","Tempo: $tempo")
+            }*/
+
 
 //            MinhaClasse().start()
-            Thread(MinhaRunnable()).start()
+//            Thread(MinhaRunnable()).start()
             /*Thread{
                 repeat(30){indice ->
                     Log.i("info_thread","MinhaClasse: $indice T: ${Thread.currentThread().name}")
@@ -50,6 +113,54 @@ class MainActivity : AppCompatActivity() {
             }.start()*/
 
         }
+
+    }
+
+    private suspend fun tarefa1(): String{
+        repeat(5){indice ->
+            Log.i("info_coroutine","tarefa1: $indice T: ${Thread.currentThread().name}")
+            delay(1000L)
+        }
+        return "Executou tarefa 1"
+    }
+
+    private suspend fun tarefa2(): String{
+        repeat(3){indice ->
+            Log.i("info_coroutine","tarefa2: $indice T: ${Thread.currentThread().name}")
+            delay(1000L)
+        }
+        return "Executou tarefa 2"
+    }
+    private suspend fun executar(){
+        repeat(15){indice ->
+            Log.i("info_coroutine","Executando: $indice T: ${Thread.currentThread().name}")
+
+            withContext(Dispatchers.Main){
+                binding.btnIniciar.text = "Executando: $indice T: ${Thread.currentThread().name}"
+                binding.btnIniciar.isEnabled = false
+            }
+            delay(1000L)
+        }
+    }
+
+    private suspend fun dadosUsuarios(){
+        val usuario = recuperarUsuarioLogado()
+        Log.i("info_coroutine","Usuario: ${usuario.nome} T: ${Thread.currentThread().name}")
+        val postagem = recuperarPostagensPeloId(usuario.id)
+        Log.i("info_coroutine","postagens: ${postagem.size} T: ${Thread.currentThread().name}")
+    }
+    private suspend fun recuperarPostagensPeloId(idUsuario: Int): List<String>{
+        delay(2000)
+        return listOf(
+            "Viagem",
+            "Estudando",
+            "Jantando"
+        )
+    }
+    private suspend fun recuperarUsuarioLogado(): Usuario{
+
+        delay(2000)
+        return Usuario(1020,"Leonardo Rigonini")
 
     }
 
